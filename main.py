4fba0e5f1b5c9b5910637c4a8c5bde6efeceb19a
jmtotah <jtotah@msudenver.edu>
2019-04-28 18:01:44 -0600

    Python classes with bonus material. Comments included.

--- main.py
+++ main.py
@@ -1,24 +1,98 @@
-from stack import Stack
+from stack import * #Import everything regarding class Stack
 
 
 def test_integers():
-    s = Stack()
-    s.push(5)
-    t = s.pop()
-    print(t)
+    print("int test")
+    s = Stack(7) #Class Stack for integer inputs
+    s.push(5) #Put 5 into the list stack
+    t = s.pop() #Remove 5
+    print(t) #Show result
 
 
 def test_floats():
-    pass
+    print("float test")
+    s = Stack(2.3) #Class Stack for float inputs
+    s.push(5.0) #Put 5.0 into the list stack
+    t = s.pop() #Remove 5.0
+    print(t) #Show result
+
+
 
 
 def test_strings():
-    pass
+    print("string test")
+    s = Stack("") #Class Stack for string inputs
+    s.push("Sharks suck") #Put string into the list stack
+    t = s.pop() #Remove string
+    print(t) #Show result
 
 
-def test_all():
-    pass
 
+def test_exception():
+    print("exception test")
+    s = Stack(5) #Class Stack for integer inputs
+    s.push("Sharks suck") #Not an integer so freak out!
+    t = s.pop() #Remove string
+    print(t) #Show result
+
+def safe_test_int_exception(): #Only keep ints out
+    print("safe int test")
+    s = IntStack()
+    s.push(1.0)
+
+
+def safe_test_int(): #Only lets ints in
+    print("safe non int test")
+    s = IntStack()
+    s.push(5)
 
 if __name__ == '__main__':
-    test_integers()
+
+
+
+     try:
+         test_exception() #Run the test exception function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("exception test value error") #Print this error
+
+
+     try:
+         test_strings() #Run the test strings function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("string test value error") #Print this error
+
+
+
+     try:
+         test_integers() #Run the test integers function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("integer test value error") #Print this error
+
+
+
+     try:
+         test_floats() #Run the test floats function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("float test value error") #Print this error
+
+
+
+
+     try:
+         safe_test_nonint() #Run the safe test for non ints function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("safe test non int value error") #Print this error
+
+
+     try:
+         safe_test_int() #Run the safe test for ints function
+
+     except ValueError: #If the value doesn't match the stack value
+         print("safe test int value error") #Print this error
+         
+
--- stack.py
+++ stack.py
@@ -1,12 +1,34 @@
+
+
 class Stack():
-    def __init__(self):
+    def __init__(self, sample): #Paramiters for Stack
+        self.stack = [] #List that gets added to
+        self.sample = sample #Self needed so that the def's can comunnicate
         print("New Stack object")
-        pass
 
-    def pop(self):
+
+    def pop(self): #For removing items from the list
         print("Popping...")
-        return None
+        return self.stack.pop()
 
-    def push(self, value):
-        print("Pushing...")
-        pass
+    def push(self, value): #For adding items to the list
+        if not isinstance(value, type(self.sample)): #If the value inputted is not the same type as sample
+            raise ValueError('Expected integer but got {}'.format(type(value))) #Show this error
+        else:
+            self.stack.append(value) #Otherwise add it to the list
+            print("Pushing...") #And print pushing
+
+
+
+class IntStack(Stack): #To allow for int only check
+    def __init__(self):
+        Stack.__init__(self,0)
+
+
+
+
+
+
+
+
+
